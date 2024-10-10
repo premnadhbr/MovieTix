746747509c5be3a8addad2bb519ea21c40c7b8c6
premnadh <premnathbr123@gmail.com>
2024-10-10 12:53:34 +0530

    bug fixed

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,9 @@
 import 'package:cinema_ticket_booking_app/utils/const.dart';
 import 'package:cinema_ticket_booking_app/view/mainscreen/cinema_main_screen.dart';
-import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(DevicePreview(
-    builder: (context) => const MyApp(),
-    enabled: true,
-  ));
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
